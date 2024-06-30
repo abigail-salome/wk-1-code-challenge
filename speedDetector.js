c9436d437799d603b8c157e947444aa2100fb913
Salome Abigail <salomeabigail9@gmail.com>
2024-06-30 16:00:56 +0300

    created studentGradeGenerator file

--- speedDetector.js
+++ speedDetector.js
@@ -1,1 +1,38 @@
-// code
+
+function detector(speed) {
+    // Defines the speed limit
+    const speedLimit = 70
+    // Checks if the speed is within the limit or not
+    const kmPerDemeritPoint = 5;
+if (speed <= 70) {
+    return "Ok";
+} else if (speed <= 75) {
+    return "Points: 1";
+} else if (speed <= 80) {
+    return "Points: 2";
+} else if (speed <= 85) {
+    return "Points: 3";
+} else if (speed <= 90) {
+    return "Points: 4";
+} else if (speed <= 95) {
+    return "Points: 5";
+} else if (speed <= 100) {
+    return "Points: 6";
+} else if (speed <= 105) {
+    return "Points: 7";
+} else if (speed <= 110) {
+    return "Points: 8";
+} else if (speed <= 115) {
+    return "Points: 9";
+} else if (speed <= 120) {
+    return "Points: 10";
+} else if (speed <= 125) {
+    return "Points: 11";
+} else if (speed <= 130) {
+    return "Points: 12";
+} else {
+    return "License suspended."
+}
+}
+// Testing the function with a speed of 80
+detector(80);
--- studentGradeGenerator.js
+++ studentGradeGenerator.js
@@ -1,31 +1,35 @@
+const userInput = require("node:readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // Function to determine the grade based on the marks input
 function determineGrade(marks) {
-     // Grades are determined based on specific ranges of marks
-    if (marks > 79 ) {
-        return "A";
-    } else if (marks >= 60 && marks <= 79) {
-        return "B";
-    } else if (marks >= 49 && marks <=59 ) {
-        return "C"; 
-    } else if (marks >= 40 && marks <= 49) {
-        return "D";
-    } else if (marks < 40 ) {
-        return "E";
+  // Grades are determined based on specific ranges of marks
+  if (marks > 79) {
+    return "A";
+  } else if (marks >= 60 && marks <= 79) {
+    return "B";
+  } else if (marks >= 49 && marks <= 59) {
+    return "C";
+  } else if (marks >= 40 && marks <= 49) {
+    return "D";
+  } else if (marks < 40) {
+    return "E";
+  }
+}
+
+
+  
+
+userInput.question("enter student marks: ", function(input) {
+    const marks = parseInt(input)
+
+    if (isNaN(marks) || marks > 100 || marks < 0) {
+        console.log("you entered an invalid mark")
+    } else {
+        const grade = determineGrade(marks)
+        console.log(`the grade is ${grade}`)
     }
-    }
-    // Function to test and display the grade for a given mark
-    function testGrade(mark) {
-        // Check if the mark is a valid number within the range of 0 to 100
-        if (! isNaN(mark) && mark >= 0 && mark <= 100) {
-             // Calculate and store the grade using the determineGrade function
-            let grade = determineGrade(mark);
-            // Output the grade based on the mark
-            console.log("for mark", mark + ", the student's grade is :" , grade);
-        } else {
-            // Output an error message if the mark is not within the valid range
-            console.log("ERROR: marks should be between 0 and 100")
-        }
-    }
-    // Testing the function with a mark of 55
-    testGrade(55);
+    userInput.close()
+})
